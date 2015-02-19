b7a74b2ee13c752ca4edc2ac1394b778289a6adb
Ryan Thompson <service@aiwebsystems.com>
2015-02-19 12:48:34 -0600

    Test

---
+++ src/ExampleThemeServiceProvider.php
@@ -0,0 +1,31 @@
+<?php namespace Anomaly\ExampleTheme;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ExampleThemeServiceProvider
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\ExampleTheme
+ */
+class ExampleThemeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        app('router')->get(
+            '/',
+            function () {
+
+                die('Test');
+            }
+        );
+    }
+}
